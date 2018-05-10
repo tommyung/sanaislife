abd81d6df2ea45e60545c090909d6ab2684c9bda
tommyung <39108126+tommyung@users.noreply.github.com>
2018-05-09 22:16:50 -0400

    Update maplestory2bot.py

--- maplestory2bot.py
+++ maplestory2bot.py
@@ -9,9 +9,16 @@ async def on_message(message):
     # we do not want the bot to reply to itself
     if message.author == client.user:
         return
-
+    
     if message.content.startswith('!raid'):
-        print(message.content)
+        msgContent = message.content
+        msgSplit = msgContent.split()
+        for tmp in msgSplit:
+        #to be determined on how its formatted
+        #!raid create bossName creatorName time
+        if msgData.lower() in ['create']:
+             row = [msgSplit[2], msgSplit[3], msgSplit[4]]
+             sheet.insert_row(row, 2)
         scope = ['http://spreadsheets.google.com/feeds',
                 'https://www.googleapis.com/auth/drive']
         creds = ServiceAccountCredentials.from_json_keyfile_name('Raiding form-b21d7c952538.json', scope)

36cb71a73f74959560b5b123d7b08efeff670b52
tommyung <39108126+tommyung@users.noreply.github.com>
2018-05-09 20:58:31 -0400

    Update maplestory2bot.py

--- maplestory2bot.py
+++ maplestory2bot.py
@@ -11,7 +11,11 @@ async def on_message(message):
         return
 
     if message.content.startswith('!hello'):
-        list_of_hashes = sheet.get_all_records()
+        scope = ['http://spreadsheets.google.com/feeds']
+        creds = ServiceAccountCredentials.from_json_key_name('Raiding form-b21d7c952538', scope)
+        client = gspread.authorize(creds)
+        sheet = client.open("Raiding form").sheet1
+        list_of_hashes = sheet.get_all_records()        
         msg = 'Hello {0.author.mention}'.format(message)
         msg = (list_of_hashes)
         await client.send_message(message.channel, msg)
@@ -25,8 +29,3 @@ async def on_ready():
     
 tokenAddress = os.environ['token'] 
 client.run(tokenAddress)
-
-scope = ['http://spreadsheets.google.com/feeds']
-creds = ServiceAccountCredentials.from_json_key_name('Raiding form-b21d7c952538', scope)
-client = gspread.authorize(creds)
-sheet = client.open("Raiding form").sheet1

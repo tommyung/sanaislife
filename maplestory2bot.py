a43238e915a23f33e958f94d373bed324053e97a
tommyung <39108126+tommyung@users.noreply.github.com>
2018-09-01 15:20:21 -0400

    Update maplestory2bot.py

--- maplestory2bot.py
+++ maplestory2bot.py
@@ -1,6 +1,6 @@
 import discord
-#import gspread
-#from oauth2client.service_account import ServiceAccountCredentials
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 import os
 
 client = discord.Client()
@@ -9,30 +9,30 @@ client = discord.Client()
 async def on_member_join(member):
     role = discord.utils.get(member.server.roles, name='Interviewee')
     await client.add_roles(member,role)
-#@client.event
-#async def on_message(message):
-    # we do not want the bot to reply to itself
-#    if message.author == client.user:
-#        return
+@client.event
+async def on_message(message):
     
-#    if message.content.startswith('!raid'):
-#        msgContent = message.content
-#        msgSplit = msgContent.split()
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+    if message.content.startswith('!raid'):
+        msgContent = message.content
+        msgSplit = msgContent.split()
         #to be determined on how its formatted
         #!raid create yourName bossName time
-#        scope = ['http://spreadsheets.google.com/feeds',
-#                'https://www.googleapis.com/auth/drive']
-#        creds = ServiceAccountCredentials.from_json_keyfile_name('Raiding form-b21d7c952538.json', scope)
-#        clientName = gspread.authorize(creds)
-#        sheet = clientName.open("Raiding form").sheet1
-#        if msgSplit[1].lower() == 'create':
-#             row = [msgSplit[2], msgSplit[3], msgSplit[4]]
-#             sheet.insert_row(row, 2)
-#             raidName = sheet.cell(2,2).value
-#             msgRaid = '{0.author.mention} has created a raid sign up for' + raidName
+        scope = ['http://spreadsheets.google.com/feeds',
+                'https://www.googleapis.com/auth/drive']
+        creds = ServiceAccountCredentials.from_json_keyfile_name('Raiding form-b21d7c952538.json', scope)
+        clientName = gspread.authorize(creds)
+        sheet = clientName.open("Raiding form").sheet1
+        if msgSplit[1].lower() == 'create':
+            row = [msgSplit[2], msgSplit[3], msgSplit[4]]
+             sheet.insert_row(row, 2)
+             raidName = sheet.cell(2,2).value
+             msgRaid = '{0.author.mention} has created a raid sign up for' + raidName
         
-#        else:
-#            msgRaid = '{0.author.mention}, you entered an invalid command. \nTo create a raid, type "!raid create [IGN] [Boss] [Time]"\ne.g. !raid create Sana Zakum 5:00PM_EST\n\nTo view avaliable raids, type "!raid show"'
+        else:
+            msgRaid = '{0.author.mention}, you entered an invalid command. \nTo create a raid, type "!raid create [IGN] [Boss] [Time]"\ne.g. !raid create Sana Zakum 5:00PM_EST\n\nTo view avaliable raids, type "!raid show"'
         #elif msgSplit[1].lower() == 'list':
         #    for x in range(5):
         #        msgList = sheet.get_all_values()
@@ -40,8 +40,8 @@ async def on_member_join(member):
                 #msgTmp = sheet.row_values(x)
                 #msgList = msgList + "\n" + msgTmp
         #        msgRaid = '{0.author.mention} the list of raids: \n' + msgString
-#        msg = str(msgRaid).format(message)
-#        await client.send_message(message.channel, msg)
+        msg = str(msgRaid).format(message)
+        await client.send_message(message.channel, msg)
 
 @client.event
 async def on_ready():

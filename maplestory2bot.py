dcb202fb51c122cb9eb9d5994aefd2b89e2a59af
Jason Nguyen <jason.nguyen1294@gmail.com>
2019-01-15 02:48:10 -0600

    added some code for create, join and list. function to iterate through object of people for a list to send as message

--- maplestory2bot.py
+++ maplestory2bot.py
@@ -8,6 +8,12 @@ raidDict = {}
 client = discord.Client()
 derogatoryList = ["whore", "nigger", "nigga", "chink", "nigguh", "niggar", "beaner"]
 
+def raidList(raid):
+    returnString = raid['boss'] + ' ' + raid['time'] + ' lead by ' + raid['author'] + '\n'
+    for x in range(0, raid['count']):
+            returnString += str(x) + '. ' + raid[x] + '\n'
+    return returnString
+
 @client.event
 async def on_member_join(member):
     role = discord.utils.get(member.server.roles, name='Interviewee')
@@ -17,50 +23,79 @@ async def on_member_join(member):
 async def on_message(message):
     if message.author == client.user:
         return
-   
+
     if message.content.startswith('!time'):
         msgTime = datetime.datetime.now().time()
         timeSplit = (str(msgTime).split(':'))
         combineTime = timeSplit[0] + ':' + timeSplit[1]
         resetHour = 23 - int(timeSplit[0])
         resetMinute = 60 - int(timeSplit[1])
-        msgContent = '{0.author.mention} server time is ' + str(timeSplit[0]) + ':' + str(timeSplit[1]) + '\nServer reset in ' + str(resetHour) + ' hour(s) and ' + str(resetMinute) + ' minute(s)' 
+        msgContent = '{0.author.mention} server time is ' + str(timeSplit[0]) + ':' + str(timeSplit[1]) + '\nServer reset in ' + str(resetHour) + ' hour(s) and ' + str(resetMinute) + ' minute(s)'
         msg = str(msgContent).format(message)
         await client.send_message(message.channel, msg)
-        
-        
+
+
     if message.content.startswith('!quack'):
         msg = 'https://www.youtube.com/watch?v=QKq42dE6cvI'
         await client.send_message(message.channel, msg)
-        
-        
+
+
     if message.content.startswith('!raid'):
         msgContent = message.content
         msgSplit = msgContent.split()
-        if 'Raid master' in author.roles:
-            if msgSplit[1].lower() == 'create':
+        command = msgSplit[1].lower()
+        raidName = msgSplit[2].lower()
+        if 'Raid master' in [y.name for y in message.author.roles]:
+            if command == 'create':
                 #!raid create <raidName> <boss> <time>
-                raidDict[msgSplit[2].lower()] = message.author, msgSplit[3].lower(), msgSplit[4].lower()
-                msg = '@🌸 A new raid has been created \nName: ' + msgSplit[2]
+                if msgSplit[2] in raidDict:
+                    msg = 'A raid with the name ' + msgSplit[2] + ' already exists.'
+                    await client.send_message(message.channel, msg)
+                elif len(msgSplit) != 5:
+                    msg = '${message.author.mention}, you have entered an invalid command. The raid commands it goes by the following\n\nTo create: !raid create <raid name> <boss> <time> Example: !raid create raid1 cdev 01/31-07:30PST\nTo join: !raid join <raid name> <class>'
+                    await client.send_message(message.channel, msg)
+                else:
+                    raidDict[raidName] = {}
+                    raidDict[raidName]['author'] = str(message.author)
+                    raidDict[raidName]['boss'] = str(msgSplit[3].lower())
+                    raidDict[raidName]['time'] = str(msgSplit[4].lower())
+                    raidDict[raidName]['count'] = 0
+                    msg = 'A new raid has been created \nName: ' + msgSplit[2]
+                    await client.send_message(message.channel, msg)
+            elif command == 'join':
+                #!raid join <raidName> <Class>
+                raidDict[msgSplit[2].lower()]['count'] = str(message.author) + ' ' + msgSplit[3]
+                raidDict[msgSplit[2].lower()]['count'] += 1
+                msg = raidList(raidDict[msgSplit[2].lower()])
                 await client.send_message(message.channel, msg)
-            elif msgSplit[1].lower() == 'remove':
+            elif command == 'remove':
                 #!raid remove <raidName>
                 raidDict.pop(msgSpilt[2].lower())
-            elif msgSplit[1].lower() == 'clear':
+            elif command == 'clear':
                 #!raid clear
-                riadDict.clear()
+                raidDict.clear()
         else:
-            if msgSplit[1].lower() == 'create':
-                msg = '{0.author.mention} only those who has the role Raid master can create a listing for raids'
+            if command == 'create':
+                msg = '{0.mention} only those who has the role Raid master can create a listing for raids'.format(message.author)
                 await client.send_message(message.channel, msg)
-            elif msgSplit[1].lower() =='join':
+            elif command == 'join':
                 #!raid join <raidName> <Class>
-                appendDict = str(raidDict[msgSplit[2].lower()]) + ' ' + message.author + msgSplit[3] + ', '
-                raidDict[msgSplit[2].lower()] = appendDict
+                raidDict[msgSplit[2].lower()]['count'] = str(message.author) + ' ' + msgSplit[3]
+                raidDict[msgSplit[2].lower()]['count'] += 1
+                msg = raidList(raidDict[msgSplit[2].lower()])
+                await client.send_message(message.channel, msg)
                 #Maybe using another key to keep tabs on the amount of people that joined the raid and cannot exceed the number the user specified?
-    else:
-        msg = '{0.author.mention, you have entered an invalid command. The raid commands it goes by the following\n\nTo create: !raid create <raid name> <boss> <time>\nTo join: !raid join <raid name> <class>'
-        await client.send_message(message.channel, msg)
+            elif command == 'show':
+                #!raid join <raidName>
+                if msgSplit[2].lower() in raidDict:
+                    msg = raidList(raidDict[[msgSplit[2].lower()]])
+                    await client.send_message(message.channel, msg)
+                else:
+                    msg = msgSplit[2] + ' is not a scheduled raid.'
+                    await client.send_message(message.channel, msg)
+            else:
+                msg = '{0.mention}, you have entered an invalid command. The raid commands it goes by the following\n\nTo create: !raid create <raid name> <boss> <MM/DD-HH:mmPST> Example: !raid create raid1 cdev 01/31-07:30PST\nTo join: !raid join <raid name> <class>'.format(message.author)
+                await client.send_message(message.channel, msg)
     for derogatoryTerms in derogatoryList:
         if derogatoryTerms in message.content:
             await client.delete_message(message)
@@ -68,6 +103,6 @@ async def on_message(message):
 @client.event
 async def on_ready():
     print('Bot has started...')
-    
-tokenAddress = os.environ['token'] 
+
+tokenAddress = os.environ['token']
 client.run(tokenAddress)
